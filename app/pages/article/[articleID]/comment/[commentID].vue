7aeab3b6dc25b3a91dc29d8256792004ea18cdbe
xiaofu <2501292111@qq.com>
2026-08-15 23:24:32 +0800

    多级嵌套

---
+++ app/pages/article/[articleID]/comment/[commentID].vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<template>
+  article/{{route.params.articleID}}/comment/{{route.params.commentID}}
+</template>
+
+<style scoped>
+
+</style>
---
+++ app/pages/article/[id].vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import {useRoute} from "vue-router";
+const route = useRoute()
+console.log(route,'router',route.params,route.query)
+const articleId = route.params.id
+
+definePageMeta({
+  validate: async (route) => {
+    // 检查 id 是否为数字
+    return /^\d+$/.test(route.params.id as string) // 匹配失败会跳转到404页面
+  }
+})
+</script>
+
+<template>
+  <div>
+    文章详情页面： 文章id {{articleId }}
+    查询参数：{{route.query}}
+  </div>
+</template>
