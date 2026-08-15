ed3f14534696dbdf42472c71154fa5844064e54e
xiaofu <2501292111@qq.com>
2026-08-15 23:24:20 +0800

    嵌套路由 解释：如果存在pages/parent.vue和pages/parent/child.vue，那么parent.vue内部也需要一个<NuxtPage />来承载子页面内容。

---
+++ app/pages/user.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<!--user-->
+  <nuxt-page></nuxt-page>
+</template>
+
+<style scoped>
+
+</style>
---
+++ app/pages/user/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+user/index
+</template>
+
+<style scoped>
+
+</style>
